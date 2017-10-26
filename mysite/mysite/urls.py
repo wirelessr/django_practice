ba532c2012d022ca6802fc2dda78269864289c81
Wirelessr <ifloveyou2014@gmail.com>
2017-10-26 12:54:08 +0000

    My hello page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from mysite.views import hello
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', hello),
+    url(r'^$', hello),
 ]
