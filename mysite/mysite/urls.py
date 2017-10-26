26ec23347a5e99ac2037324ce51cd0a7e95d3e1c
Wirelessr <ifloveyou2014@gmail.com>
2017-10-26 13:18:01 +0000

    Add datetime

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from mysite.views import hello
+from mysite.views import hello, current_datetime
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello),
-    url(r'^$', hello),
+    url(r'^$', hello), # homepage
+    url(r'^time/$', current_datetime),
 ]
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello world") 
+    return HttpResponse("Hello world")
+
+def current_datetime(request):
+    import datetime
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
