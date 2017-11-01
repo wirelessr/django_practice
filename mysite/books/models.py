6f6b3bf7f18733e7c5475d0f2c961c138caa6453
Wirelessr <ifloveyou2014@gmail.com>
2017-11-01 12:49:07 +0000

    Register models

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -12,6 +12,9 @@ class Publisher(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']
+
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
@@ -24,7 +27,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.title
