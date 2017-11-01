134cded48e44c04b60dfee7f5d29c96ef18ac726
Wirelessr <ifloveyou2014@gmail.com>
2017-11-01 12:57:31 +0000

    Add apps

--- mysite/mysite/production_settings.py
+++ mysite/mysite/production_settings.py
@@ -35,6 +35,12 @@ TEMPLATES = [
 ]
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'books.apps.BooksConfig',
 ]
 
