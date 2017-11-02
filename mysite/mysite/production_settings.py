7a0a5f224edec4b612c4a934c7dd15c244c7f63e
Wirelessr <ifloveyou2014@gmail.com>
2017-11-02 12:56:55 +0000

    support static assert

--- mysite/mysite/production_settings.py
+++ mysite/mysite/production_settings.py
@@ -7,7 +7,17 @@ DATABASES = {
         }
 
 # Static asset configuration.
-STATIC_ROOT = 'staticfiles'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
