84c8c2d23c164e3c0d29ecdb11a01ddf506f2773
Wirelessr <ifloveyou2014@gmail.com>
2017-11-04 07:02:54 +0000

    Implement Django Forms

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from mysite.views import hello, current_datetime, hours_ahead, shortcut_time
@@ -25,4 +25,5 @@ urlpatterns = [
     url(r'^time/$', current_datetime),
     url(r'^stime/$', shortcut_time),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^', include('books.urls')),
 ]
