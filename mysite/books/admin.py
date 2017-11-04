eab9c5df836988941177c14b1c19cd9c2d56afe6
Wirelessr <ifloveyou2014@gmail.com>
2017-11-04 06:13:32 +0000

    Something changing the model presentations on admin interface

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -12,6 +12,10 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'publisher', 'publication_date')
     list_filter = ('publication_date',)
     date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
+    #fields = ('title', 'authors', 'publication_date') # The fields while adding books
+    filter_horizontal = ('authors',) # As adding books, it can filter authors
+    raw_id_fields = ('publisher',)
 
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
