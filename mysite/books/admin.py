4563c40beaf993a806da8c44a067cefe67e46beb
Wirelessr <ifloveyou2014@gmail.com>
2017-11-04 05:19:29 +0000

    Change the scheme of listing models

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Publisher, Author, Book
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
